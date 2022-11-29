778c150e56a935083c041b97f8941932e84c6d5f
llutsefer <n.kuziv2005@gmail.com>
2022-11-30 00:48:01 +0100

    Path Parameters

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data':{'name':'Sarthak'}}
+    return {'data': 'blog list'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return{'data':'all unpublished blogs'}
 
-@app.get('/about.')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data':{'1','2'}}

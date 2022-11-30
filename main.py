e2e2b2e3fc45ff4783c67ec05d18344ade2c58eb
llutsefer <n.kuziv2005@gmail.com>
2022-11-30 01:28:36 +0100

    Query Parameters

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blods
+    if published:
+        return {'data': f'{limit} published blogs from the db list'}
+    else:
+        return {'data': f'{limit} blogs from the db list'}
 @app.get('/blog/unpublished')
 def unpublished():
     return{'data':'all unpublished blogs'}

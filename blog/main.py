0ae0399bd2c1dc4d68f4e6334f4ead3f6434beb8
llutsefer <n.kuziv2005@gmail.com>
2022-12-03 15:23:59 +0100

    Update Blog

--- blog/main.py
+++ blog/main.py
@@ -26,10 +26,25 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id ==id).delete(synchronize_session=False)
+    db.query(models.Blog).filter(models.Blog.id ==id)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    # noinspection PyTypeChecker
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    # noinspection PyTypeChecker
+    blog.update(
+        {models.Blog.title : request.title, models.Blog.body : request.body}, synchronize_session=False)
+    db.commit()
+    return 'updated'
 
 @app.get('/blog')
 def all_blogs(db: Session = Depends(get_db)):
